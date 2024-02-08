156092d3dff3d4ad9d1800db217a850b0e75a2d3
jaezudev <mrmuffinzockt01@gmail.com>
2024-02-08 11:22:05 +0100

    further work on ban command (INOP)

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -6,18 +6,28 @@ module.exports = {
 		.setDescription('Bans user')
 		.addUserOption(option => option.setName('user').setDescription('User to ban').setRequired(true))
 		.addStringOption(option => option.setName('reason').setDescription('Reason for ban'))
-		.addBooleanOption(option => option.setName('delete_messages').setDescription('Reason for ban')),
+		.addBooleanOption(option => option.setName('grant_appeal').setDescription('If ban is appealable')),
 	async execute(interaction) {
+		const perpetrator = interaction.user;
 		const user = interaction.options.getUser('user');
-		let reason = interaction.options.getString('reason');
+		let reason = interaction.options.getBoolean('reason');
 		if (!reason) reason = 'No reason specified.';
+		const appealable = interaction.options.getString('grant_appeal');
 		const guild = 'Azerbaijan & South Africa Cozy Corner';
+		let banTxt = `# Connection terminated!
+* You were removed for violating the rules.
+> Reasoning - ${reason}
+> Issuing staff - <@!${perpetrator.id}>
+> Timeframe - NaN`;
+		const appealTxt = `
+* You may appeal by reaching out to <@!${perpetrator.id}> or (recommended) or by filling out [this form](https://docs.google.com/forms/d/e/1FAIpQLSclGzlmTRNfcjgy9vYUG69SjmzBh2wIhmlHdCwU8T98p3eY-w/viewform).`;
+	if (appealable) banTxt += appealTxt;
 		if (interaction.member.permissions.has([PermissionsBitField.Flags.KickMembers, PermissionsBitField.Flags.BanMembers]) || interaction.member.id === interaction.guild.ownerId) {
 			try {
 				await user.send(`You were banned from ${guild} for the following reason:\n${reason}`);
 			}
 			catch (err) {
-				console.error(`While ban occurr "Error ${err.rawError.code}: ${err.rawError.message}"`);
+				console.error(`Error ${err.rawError.code} while sending pre-ban notice. ${err.rawError.message}`);
 			}
 			await interaction.guild.members.ban(user, [0, reason]);
 			await interaction.reply({ content: ':thumbsup:', ephemeral: true });
