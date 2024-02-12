9cb39fe46e0a1c555224675c7fad71f63abd62d0
Ernest <ernestlasher57@gmail.com>
2024-02-12 08:59:35 -0600

    Moved deplying commands into index.js

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
 const dotenv = require('dotenv');
 const ON_DEATH = require('death');
 
 dotenv.config();
 
+// on death handler that simply shows the error then gets rid of itself
 ON_DEATH(function(signal, err) {
 	console.log(`Quit because of ${err}`);
 	client.destroy();
 });
-
+// this is where the fun stuff happens
 const client = new Client({
 	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildModeration],
 });
 
-client.commands = new Collection();
+const commands = [];
+// Grab all the command folders from the commands directory you created earlier
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
 		// Set a new item in the Collection with the key as the command name and the value as the exported module
 		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
+			commands.push(command.data.toJSON());
 		}
 		else {
 			console.warn(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
@@ -35,9 +38,32 @@ for (const folder of commandFolders) {
 	}
 }
 
+// Construct and prepare an instance of the REST module
+const rest = new REST().setToken(process.env.TOKEN_DEV);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(process.env.CLIENT_DEV, process.env.GUILD_DEV),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	}
+	catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
+
+//Find any event files in the events folder
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
+// excute each event file :3
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
 	const event = require(filePath);
@@ -48,5 +74,5 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
-
+// and login to Discord!
 client.login(process.env.TOKEN_DEV);
